c461d91ea4c636282df72895e5ab051e158a6e9f
Sougata <sougatadas9874@gmail.com>
2023-12-15 19:49:37 +0530

    fix: remove comment and conditionally render open cart link based on current route.

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { getTotalCartQuantity, getTotalCartPrice } from "./cartSlice";
 import { formatCurrency } from "../../utils/helpers";
 
@@ -8,17 +8,25 @@ function CartOverview() {
 
   const totalCartPrice = useSelector(getTotalCartPrice);
 
+  const location = useLocation();
+
+  const isCartPage = location.pathname === "/cart";
+
   if (!totalCartQuantity) return null;
 
   return (
-    <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
+    <div
+      className={`flex items-center ${
+        isCartPage ? "justify-center" : "justify-between"
+      } md:text-base" bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6`}
+    >
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6 ">
         <span>
           {totalCartQuantity} {totalCartQuantity > 1 ? "PIZZAS" : "PIZZA"}
         </span>
         <span>{formatCurrency(totalCartPrice)}</span>
       </p>
-      <Link to="/cart">Open cart &rarr;</Link>
+      {!isCartPage && <Link to="/cart">Open cart &rarr;</Link>}
     </div>
   );
 }
--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -19,7 +19,6 @@ export async function loader({ params }) {
 function Order() {
   const order = useLoaderData();
 
-  // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
     status,
